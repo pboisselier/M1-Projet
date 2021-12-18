56b6bd7ee1573f8f06b4d737564a8bb84ee526d1
Pierre <Raclette2K@users.noreply.github.com>
2021-12-18 12:14:35 +0100

    PCF8563 now has sync read and set

--- rpi/example/arpi600/pcf8563.c
+++ rpi/example/arpi600/pcf8563.c
@@ -10,10 +10,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpi600/pcf8563.h>
-
+#include <time.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
+	time_t tm;
+	if (pcf8563_init() != 0)
+		perror("init pcf8563");
+	if (pcf8563_read_time(&tm) != 0)
+		perror("read time");
+	printf("Is RTC running: %s\n", pcf8563_is_running(NULL) ? "yes" : "no");
 
-        return EXIT_SUCCESS;
+	printf("RTC Time: %s\n", asctime(gmtime(&tm)));
+
+	//	struct tm *tms = gmtime(&tm);
+	//	tms->tm_mday += 1;
+	//	tm = mktime(tms);
+
+	time(&tm);
+	if (pcf8563_set_time(&tm) != 0)
+		perror("unable to se time");
+
+	if (pcf8563_close() != 0)
+		perror("unable to close pcf device");
+
+	return EXIT_SUCCESS;
 }
