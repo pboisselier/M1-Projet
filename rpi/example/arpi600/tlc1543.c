9229228e20ba454070d0e882abce3256a8cb4e5e
Pierre Boisselier <Raclette2K@users.noreply.github.com>
2021-12-30 22:47:22 +0100

    Updated tlc1543 example

--- rpi/example/arpi600/tlc1543.c
+++ rpi/example/arpi600/tlc1543.c
@@ -1,32 +1,66 @@
 /**
- * @file tl1543.c
- * @author Pierre Boisselier (Raclette2K@users.noreply.github.com)
  * @brief Example using the TLC1543 library
+ * @copyright (c) Pierre Boisselier 
  * 
- * @copyright Copyright (c) 2021
- *
- * Compile:
- *      cc -I../../include tlc1543.c -std=c11 -Wall -g -lgpiod -o tlc1543 
- * 
+ * @example tlc1543.c
+ * Compile using:
+ * ```sh
+ * cc -I../../include tlc1543.c -std=c11 -Wall -g -lgpiod -o tlc1543 
+ * ```
  */
-
-#include <arpi600/tlc1543.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpi600/arpi600.h>
 
 int main(void)
 {
 	struct tlc1543 tlc;
 	int value = 0;
-	if (tlc1543_init(&tlc) < 0)
-		perror("error initializing");
-	
-	for (int i = 0; i < 100000; ++i) {
+
+	if (tlc1543_init(&tlc, TLC1543_OPT_EXCLUSIVE) < 0) {
+		perror("unable to init the TLC1543");
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 0; i < 20; ++i) {
 		value = tlc1543_get_sample(&tlc, 0);
-		if (value < 0)
-			perror("error getting a sample");
-		printf("ADC Read: %d\n", value);
+		if (value < 0) {
+			perror("unable to read from the ADC");
+		}
+		printf("ADC read: %d\n", value);
 	}
 
 	tlc1543_delete(&tlc);
 
 	return EXIT_SUCCESS;
 }
+
+/* Some tests */
+//	if (!fork()) {
+//		fflush(stdout);
+//		if (tlc1543_init(&tlc, TLC1543_OPT_WAIT) < 0) {
+//			perror("error initializing");
+//			return EXIT_FAILURE;
+//		}
+//		for (int i = 0; i < 10; ++i) {
+//			value = tlc1543_get_sample(&tlc, 0);
+//			if (value < 0)
+//				perror("error getting a sample");
+//			printf("ADC Read child: %d => %x\n", value, value);
+//			fflush(stdout);
+//		}
+//		tlc1543_delete(&tlc);
+//	} else {
+//		if (tlc1543_init(&tlc, TLC1543_OPT_EXCLUSIVE) < 0) {
+//			perror("error initializing tlc");
+//			return EXIT_FAILURE;
+//		}
+//		for (int i = 0; i < 10; ++i) {
+//			value = tlc1543_get_sample(&tlc, 13);
+//			if (value < 0)
+//				perror("error getting a sample");
+//			printf("ADC Read: %d => %x\n", value, value);
+//			fflush(stdout);
+//		}
+//		tlc1543_delete(&tlc);
+//	}
