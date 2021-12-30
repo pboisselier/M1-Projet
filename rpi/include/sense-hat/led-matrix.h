5326a4f03efdb037d382a1b43fbb93e30d431d73
Pierre Boisselier <Raclette2K@users.noreply.github.com>
2021-12-30 22:56:59 +0100

    Added led-matrix.h template

---
+++ rpi/include/sense-hat/led-matrix.h
@@ -0,0 +1,29 @@
+/**
+ * @brief Library for the 8x8 LED Matrix on the Sense-Hat
+ * 
+ * @file led-matrix.h
+ * @ingroup Sense-Hat
+ * @copyright (c) Pierre Boisselier
+ * @date 2021-12-30
+ * 
+ * @details
+ * This libary allows one to use the 8x8 LED Matrix attached to the Raspberry Pi Sense-Hat.  
+ * On the Raspberry Pi the LED Matrix is recognized as a Framebuffer with the `rpisense-fb.ko` driver.  
+ * 
+ * 
+ */
+
+#ifndef LED_MATRIX_H
+#define LED_MATRIX_H
+
+#include <sys/mman.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LED_MATRIX_H
